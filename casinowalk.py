995e13bd8306e85c8b29829c0223c3ce5994ce47
guidohuijgen <224475@lekenlinge.nl>
2026-02-06 17:28:35 +0100

    code

--- casinowalk.py
+++ casinowalk.py
@@ -1,10 +1,9 @@
 #casinowalk
 import play
+import time
 
-platform = play.new_image("platform.png",size = 75)
-platform.hide()
-
-beginscherm = play.new_image("download.png", size= 300)
+#beginscherm = play.new_image("download.png", size= 300)
+#platform = play.new_image("platform.png",size = 75)
 
 start_box = play.new_box(color = 'red',width=250, height=85)
 
@@ -15,40 +14,58 @@ reset_button.x = 270
 reset_button.y = 270
 reset_button.hide()
 
-money = 20
+money = 0
 money_button = play.new_text (f'€{money} ', color = 'white', font_size = 20)
 money_button.hide()
 money_button.x=270
 money_button.y=250
 
+reset_button = play.new_text('RESET',color = 'black', font_size = 20)
+reset_button.x = 270
+reset_button.y = 270
+reset_button.hide()
+
 player = play.new_box(color= 'black', width =45, height = 45)
 player.hide()
 
-@play.when_key_pressed("w","up")
-def vooruit_function():
-    player.y = player.y + 3
-
-@play.when_key_pressed("a")
-def links_function():
-    player.x = player.x -3
-
-@play.when_key_pressed("s","down")
-def achteruit_function():
-    player.y = player.y -3
-
-@play.when_key_pressed("d",) 
-def rechts_function():
-    player.x = player.x +3
+if money <= 0:
+    game_over = play.new_text("Je bent blut...", color = "red",font_size = 60)
+    for i in [0,4]:
+        time.sleep(1)
+        game_over.hide()
+        time.sleep(1)
+        game_over.show()
+    play.stop_program
 
 @start_box.when_clicked
 def start_function():
     start_button.hide()
     start_box.hide()
-    reset_button.show()
+    #platform.show()
+    money_button = play.new_text (f'€{money} ', color = 'white', font_size = 20)
+    money_button.x=270
+    money_button.y=250
     money_button.show()
-    platform.show()
-    beginscherm.hide()
+    reset_button.show()
+    # reset_button = play.new_text('RESET',color = 'black', font_size = 20)
+    # reset_button.x = 270
+    # reset_button.y = 270
+    #beginscherm.hide()
+    # player = play.new_box(color= 'black', width =45, height = 45)
     player.show()
+    @play.when_key_pressed("w","up")
+    def vooruit_function():
+        player.y = player.y + 2
+    @play.when_key_pressed("a","left")
+    def links_function():
+        player.x = player.x -2
+    @play.when_key_pressed("s","down")
+    def achteruit_function():
+        player.y = player.y -2
+    @play.when_key_pressed("d","right") 
+    def rechts_function():
+        player.x = player.x +2
+
 
 @reset_button.when_clicked
 def reset_function():
@@ -56,7 +73,10 @@ def reset_function():
     start_box.show()
     reset_button.hide()
     money_button.hide()
-    platform.hide()
+    #beginscherm.show()
+    #platform.hide()
+    # player = play.new_box(color= 'black', width =45, height = 45)
     player.hide()
+    
 play.start_program()
 
